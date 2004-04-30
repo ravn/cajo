1b463f1cf213d12bb3d73fb267a714e565d29bc2
John Catherino <cajo@dev.java.net>
2004-04-30 21:23:50 +0000

    Updated to include new package gnu.cajo.utils.extra!

--- javanet/doc.bat
+++ javanet/doc.bat
@@ -1,1 +1,1 @@
-javadoc -author -version -nodeprecated -d docs -sourcepath . -windowtitle "Cajo Distributed Component Architecture" gnu.cajo.invoke gnu.cajo.utils
+javadoc -author -version -nodeprecated -d docs -sourcepath . -windowtitle "Cajo Distributed Component Architecture" gnu.cajo.invoke gnu.cajo.utils gnu.cajo.utils.extra
