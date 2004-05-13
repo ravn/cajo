9d6fcd431602c33482f87cd765f97d95fb13f171
John Catherino <cajo@dev.java.net>
2004-05-13 17:59:43 +0000

    Streamlined and simplified implementation. Wished I would have called it URLWrapper though, sigh too late now. Also, trying to get this out quickly, as it could cause some backward compatibility issues for users of its former utility methods serialize() and deserialize() which are deemed superfluous.

--- javanet/gnu/cajo/utils/extra/Wrapper.java
+++ javanet/gnu/cajo/utils/extra/Wrapper.java
@@ -1,11 +1,5 @@
 package gnu.cajo.utils.extra;
 
-import java.io.IOException;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.rmi.RemoteException;
-import java.rmi.NotBoundException;
-import java.net.MalformedURLException;
 import gnu.cajo.invoke.Remote;
 import gnu.cajo.invoke.Invoke;
 import gnu.cajo.invoke.RemoteInvoke;
@@ -53,49 +47,19 @@ public class Wrapper implements Invoke {
     * object, or even an ordinary local object. It is not declared final, as
     * to allow its representation to change, as required for performance or
     * other application specific reasons, for example to be further wrapped by
-    * a {@link gnu.cajo.utils.MonitorItem MonitorItem}.
+    * a {@link gnu.cajo.utils.MonitorItem MonitorItem}. It is transient to
+    * force subclasses to manage application specific persistence.
     */
-   protected Object object;
+   protected transient Object object;
    /**
-    * This method is used to save the current state of the internal object as
-    * a zipped MarshalledObject (zedmob) to a file. A subclass could override
-    * this method, to write the zedmob to a socket, for remote storage, or
-    * simply notify a remote item to save its state. It will obtain a
-    * synchronization lock on the object before writing, to prevent storage of
-    * a partially updated state. <i>Note:</i> the internal object must have its
-    * non-threadsafe methods, or blocks, synchronized.
-    * @param fileName The path/file.ext in which to save the zedmob. If it does
-    * not exist it will be created. If it exists, it will be overwritten.
-    * @throws FileNotFoundException If the file name is invalid, or the name
-    * of a directory.
-    * @throws IOException If the write operation is prohibited, or failed.
-    * @throws NotSerializableException If the inner object is not serializable.
+    * The URL where to get the wrapped object: file://, http://, ftp://,
+    * /path/name, path/name, or //[host][:port]/[name]. The host, port,
+    * and name, are all optional. If missing the host is presumed local, the
+    * port 1099, and the name "main". The referenced resource can be
+    * returned as a MarshalledObject, it will be extracted automatically.
+    * If the URL is null, it will be assumed to be ///.
     */
-   protected void serialize(String fileName) throws IOException {
-      synchronized(object) {
-         Remote.zedmob(new FileOutputStream(fileName), object);
-      }
-   }
-   /**
-    * This method is used to restore the current state of the internal object
-    * from a zipped MarshalledObject (zedmob) in a file. If the internal
-    * object already exists, it will be overwritten. This can be useful for the
-    * performance of <i>'undo'</i> operations. A subclass could override this
-    * method, to read the zedmob from a socket, from remote storage, or simply
-    * request an current item reference or proxy.
-    * @param fileName The path/file.ext from which to load the zedmob.
-    * @throws FileNotFoundException If the file name is invalid, or the name
-    * does not match an existing file.
-    * @throws IOException If the read operation is prohibited, or failed.
-    * @throws ClassNotFoundException If the stored object's codebase cannot be
-    * found.
-    * @throws StreamCorruptedException If control information in the stream is
-    * not consistent.
-    */
-   protected void deserialize(String fileName)
-      throws IOException, ClassNotFoundException {
-      object = Remote.zedmob(new FileInputStream(fileName));
-   }
+   protected String url;
    /**
     * The no-arg constructor does nothing, it is protected for use only by
     * subclasses.
@@ -116,55 +80,71 @@ public class Wrapper implements Invoke {
     * known at compile time, therefore proper operation if this VM is behind a
     * firewall could be blocked. Use behind a firewall would require knowing
     * all the ports that would be needed in advance, and enabling them before
-    * loading the item.
+    * loading the item. <i>Note:</i> for efficiency, the item will <u>not</u>
+    * be loaded at this time, rather on the first time it is need to perform
+    * in a particular method.
     * @param url The URL where to get the object: file://, http://, ftp://,
     * /path/name, path/name, or //[host][:port]/[name]. The host, port,
     * and name, are all optional. If missing the host is presumed local, the
     * port 1099, and the name "main". The referenced resource can be
     * returned as a MarshalledObject, it will be extracted automatically.
     * If the URL is null, it will be assumed to be ///.
-    * @throws RemoteException if the remote registry could not be reached.
-    * @throws NotBoundException if the requested name is not in the registry.
-    * @throws IOException if the zedmob format is invalid.
-    * @throws ClassNotFoundException if a proxy was sent to the VM, and
-    * proxy hosting was not enabled.
-    * @throws InstantiationException when the URL specifies a class name
-    * which cannot be instantiated at runtime.
-    * @throws IllegalAccessException when the url specifies a class name
-    * and it does not support a no-arg constructor.
-    * @throws MalformedURLException if the URL is not in the format explained
     */
-   public Wrapper(String url) throws RemoteException,
-      NotBoundException, IOException, ClassNotFoundException,
-      InstantiationException, IllegalAccessException, MalformedURLException {
-      object = Remote.getItem(url);
-   }
+   public Wrapper(String url) { this.url = url; }
    /**
     * This method returns the hashcode of the inner object instead of
     * the wrapper itself. This allows two different wrappers referencing an
-    * equivalent inner object to hash identically.
+    * equivalent inner object to hash identically. If the wrapped object has
+    * not yet been loaded, this will cause it to happen, which could
+    * result in a NullPointerException on a load failure.
     */
-   public int hashCode() { return object.hashCode(); }
+   public int hashCode() {
+      if (object == null) try { object = Remote.getItem(url); }
+      catch(Exception x) {} 
+      return object.hashCode();
+   }
    /**
     * This method returns checks for equality with the inner object instead of
     * the wrapper itself. This allows two different wrappers referencing an
-    * equivalent inner object to return true.
+    * equivalent inner object to return true. If the wrapped object has
+    * not yet been deserialized, this will cause it to happen, which could
+    * result in a NullPointerException on failure. If the wrapped object has
+    * not yet been loaded, this will cause it to happen, which could
+    * result in a NullPointerException on a load failure.
     */
-   public boolean equals(Object o) { return o.equals(object); }
+   public boolean equals(Object o) {
+      if (object == null) try { object = Remote.getItem(url); }
+      catch(Exception x) {} 
+      return o.equals(object);
+   }
    /**
     * This method returns the toString result of the inner object instead of
     * the wrapper itself. This allows two different wrappers referencing an
-    * equivalent inner object to return the equivalent strings.
+    * equivalent inner object to return an equivalent string. If the wrapped
+    * object has not yet been deserialized, this will cause it to happen, which
+    * could result in a NullPointerException on failure. If the wrapped object
+    * has not yet been loaded, this will cause it to happen, which could
+    * result in a NullPointerException on a load failure.
     */
-   public String toString() { return object.toString(); }
+   public String toString() {
+      if (object == null) try { object = Remote.getItem(url); }
+      catch(Exception x) {} 
+      return object.toString();
+   }
    /**
     * This method is used to test if the inner object is a reference to a
     * remote object. This can be important to know as remote invocations are
     * not time deterministic, and not assured of execution, as with local
-    * objects.
+    * objects. If the wrapped object has not yet been loaded, this will cause
+    * it to happen, which could result in a NullPointerException on a load
+    * failure.
     * @return True if the inner object is remote, false otherwise.
     */
-   public boolean isRemote() { return object instanceof RemoteInvoke; }
+   public boolean isRemote() {
+      if (object == null) try { object = Remote.getItem(url); }
+      catch(Exception x) {} 
+      return object instanceof RemoteInvoke;
+   }
    /**
     * This method attempts to extract usage information about the inner object,
     * if the method is supported.
@@ -179,21 +159,32 @@ public class Wrapper implements Invoke {
       return (String)invoke("getDescription", null);
    }
    /**
-    * This method is used to bypass the normal compiletime type checking, and
-    * execute methods directly on the inner object by name. It is primarily
-    * used to support scripted invocations on an object, typically from a text
-    * file. <i>Note:</i> Strong typechecking is still enforced, merely it must
-    * be moved from compiletime to runtime.
+    * This method <u><i>must</i></u> be called by all interface methods of the
+    * subclass, as it will load the wrapped object if it has not yet been
+    * loaded.
     * @param method The method name to be invoked.
     * @param args The arguments to provide to the method for its invocation,
     * possibly null.
     * @return The resulting data, from the invocation, possibly null.
+    * @throws RemoteException if the remote registry could not be reached.
+    * @throws NotBoundException if the requested name is not in the registry.
+    * @throws IOException if the zedmob format is invalid.
+    * @throws InstantiationException when the URL specifies a class name
+    * which cannot be instantiated at runtime.
+    * @throws IllegalAccessException when the url specifies a class name
+    * and it does not support a no-arg constructor.
+    * @throws MalformedURLException if the URL is not in the format explained
     * @throws IllegalArgumentException If the method argument is null.
     * @throws NoSuchMethodException If no matching method can be found.
+    * @throws ClassNotFoundException If the wrapped object's codebase cannot be
+    * found on deserialization.
+    * @throws StreamCorruptedException If control information in the stream is
+    * not consistent on deserialization.
     * @throws Exception If the inner object rejected the invocation, for
     * any application specific reasons.
     */
    public Object invoke(String method, Object args) throws Exception {
+      if (object == null) Remote.getItem(url);
       return Remote.invoke(object, method, args);
    }
 }
