1aecd5f14b4c3938064f62ee734b1bda97796d5f
John Catherino <cajo@dev.java.net>
2005-06-04 16:53:58 +0000

    corrected, not to include ProxyLoader.class - for proxy codebases only

--- javanet/library.bat
+++ javanet/library.bat
@@ -4,6 +4,7 @@ javac gnu\cajo\invoke\*.java
 javac gnu\cajo\utils\*.java
 javac gnu\cajo\utils\extra\*.java
 rmic  -v1.2 gnu.cajo.invoke.Remote
+del   gnu\cajo\utils\ProxyLoader.class
 jar   cf cajo.jar gnu\cajo\invoke\*.class gnu\cajo\utils\*.class gnu\cajo\utils\extra\*.class
 del   gnu\cajo\invoke\*.class
 del   gnu\cajo\utils\*.class
