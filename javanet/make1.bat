0e5e7d7f884ce07cd66b52508aab139fa69c1962
John Catherino <cajo@dev.java.net>
2005-08-22 23:00:10 +0000

    added source=1.2 and target=1.2 instructions

--- javanet/make1.bat
+++ javanet/make1.bat
@@ -1,13 +1,13 @@
 rem build a monolithic proxy server:
 
 rem build the client:
-javac gnu\cajo\invoke\Client.java
+javac -target 1.2 -source 1.2 gnu\cajo\invoke\Client.java
 rmic  -v1.2 gnu.cajo.invoke.Remote
 jar   cfm client.jar client.mft gnu\cajo\invoke\*.class
 del   gnu\cajo\invoke\*.class
 
 rem build the proxy:
-javac example\Builder.java
+javac -target 1.2 -source 1.2 example\Builder.java
 java  example.Builder
 jar   cf proxy.jar example\*.class example\include\*.* example\gui\*.class gnu\cajo\utils\*.class
 del   example\*.class
@@ -17,9 +17,9 @@ del   gnu\cajo\invoke\*.class
 del   gnu\cajo\utils\*.class
 
 rem build the server:
-javac example\Main.java
+javac -target 1.2 -source 1.2 example\Main.java
 rmic  -v1.2 gnu.cajo.invoke.Remote
-jar cfm server.jar example\example.mft example\*.class gnu\cajo\invoke\*.class gnu\cajo\utils\*.class proxy.jar client.jar
+jar   cfm server.jar example\example.mft example\*.class gnu\cajo\invoke\*.class gnu\cajo\utils\*.class proxy.jar client.jar
 del   example\*.class
 del   gnu\cajo\invoke\*.class
 del   gnu\cajo\utils\*.class
