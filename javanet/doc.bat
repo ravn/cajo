c10d95a9407308bfc54f09a398e8b22d7e5ec208
John Catherino <cajo@dev.java.net>
2006-02-23 00:05:45 +0000

    create documentation subdirectory as needed

--- javanet/doc.bat
+++ javanet/doc.bat
@@ -1,1 +1,2 @@
+mkdir docs
 javadoc -author -d docs -sourcepath . -windowtitle "Cajo Distributed Component Architecture" -overview gnu/cajo/overview.html gnu.cajo.invoke gnu.cajo.utils gnu.cajo.utils.extra
