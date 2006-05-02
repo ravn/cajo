427c23d0515ebeb8a638f83854ba165ffe66cfff
John Catherino <cajo@dev.java.net>
2006-05-02 01:35:59 +0000

    modified to produce an executable library cajo.jar

--- javanet/library.bat
+++ javanet/library.bat
@@ -6,7 +6,7 @@ javac -classpath . -g:none -target 1.2 -source 1.2 gnu\cajo\utils\extra\*.java
 rmic  -classpath . -v1.2 gnu.cajo.invoke.Remote
 del   gnu\cajo\utils\ProxyLoader.class
 del   gnu\cajo\utils\BaseProxy*.class
-jar   cf cajo.jar gnu\cajo\invoke\*.class gnu\cajo\utils\*.class gnu\cajo\utils\extra\*.class
+jar   cfm cajo.jar cajo.mft readme.txt gnu\cajo\invoke\*.class gnu\cajo\utils\*.class gnu\cajo\utils\extra\*.class
 del   gnu\cajo\invoke\*.class
 del   gnu\cajo\utils\*.class
 del   gnu\cajo\utils\extra\*.class
