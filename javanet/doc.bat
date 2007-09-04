4ac079da1f02860de8e0888c3bb89a0505ed7fc0
John Catherino <cajo@dev.java.net>
2007-09-04 19:27:37 +0000

    add gnu.cajo package to javadocs

--- javanet/doc.bat
+++ javanet/doc.bat
@@ -1,2 +1,2 @@
 mkdir docs
-javadoc -author -d docs -sourcepath . -windowtitle "Cajo Distributed Component Architecture" -overview gnu/cajo/overview.html gnu.cajo.invoke gnu.cajo.utils gnu.cajo.utils.extra
+javadoc -author -d docs -sourcepath . -windowtitle "Cajo Distributed Component Architecture" -overview gnu/cajo/overview.html gnu.cajo gnu.cajo.invoke gnu.cajo.utils gnu.cajo.utils.extra
