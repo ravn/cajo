14656df8687257f5170e1976cb3c9b4f6b4440a6
John Catherino <cajo@dev.java.net>
2009-11-01 01:56:17 +0000

    further documentation clarification, based on preliminary community questions

--- javanet/gnu/cajo/Service.java
+++ javanet/gnu/cajo/Service.java
@@ -26,13 +26,15 @@ package gnu.cajo;
 /**
  * This class defines a proxy enabled network service. It is intended both
  * as a fundamental pattern, and an example of a service specification. It
- * is implemented by services that wish to include proxy functionality. A
+ * is implemented by services that wish to provide proxy functionality. A
  * proxy enabled network service means one that either furnishes, or accepts
  * proxies, or both.
  * <p>
- * This service would typically be used in situations where the benefit
- * is greater to send all, or a portion, of the service code to the client;
- * rather than having a lot of data being passed back or forth.
+ * This service would typically be implemented in situations where the
+ * benefit is greater to send all, or a portion, of the service <i>code</i>
+ * to the receiver; rather than having a lot of <i>data</i> being passed
+ * back or forth. This pattern is also ideal for the provision of graphical
+ * user interfaces between JVMs.
  * <p>
  * All services are defined as <i>plain-old</i> Java interfaces. Typically
  * these interfaces contain:<ul>
@@ -43,21 +45,9 @@ package gnu.cajo;
  * returns
  * <li>The collection of shared methods implemented by the service</ul>
  * <p>
- * Technically speaking:<ul>
- * <li>Service method signatures may be either static or instance methods on
- * the implementation.
- * <li>Service methods should be considered <i>referentially opaque</i>; i.e.
- * invoking the same method, with the same arguments, at different times is
- * <i>not</i> guaranteed to return the same result.
- * <li><u>All</u> service methods are <i>reentrant,</i> meaning that multiple
- * clients can be executing them, <i>simultaneously.</i>
- * <li>Whilst not explicitly declared, any service method invocations
- * could fail for network related reasons, resulting in a
- * java.rmi.RemoteException being thrown implicitly.
- * </ul><p>
  * A JVM may furnish as many service objects as it wishes. Normally, related
- * service interfaces are grouped into packages. Typically the
- * javadoc package.html file is used to provide a detailed explanation of the
+ * service interfaces are grouped into packages. Typically the javadoc
+ * package.html file is used to provide a detailed explanation of the
  * service collection architecture. The package may also include any custom
  * classes shared between the service interfaces, specifically; objects,
  * interfaces, and exceptions. Once a service interface is defined and
@@ -65,6 +55,18 @@ package gnu.cajo;
  * enhancements should be handled through <i>subclasses</i> of the
  * original service interface, to ensure backward compatibility.
  * <p>
+ * Technically speaking:<ul>
+ * <li>Service method signatures may be either static or instance methods on
+ * the implementation.
+ * <li>Service methods should be considered <i>referentially opaque</i>; e.g.
+ * invoking the same method, with the same arguments, at different times is
+ * <i>not</i> guaranteed to return the same result.
+ * <li><u>All</u> service methods are <i>reentrant,</i> meaning that multiple
+ * clients can be executing them <i>simultaneously.</i>
+ * <li>Whilst not explicitly declared, any service method invocations
+ * could fail for network related reasons, resulting in a
+ * java.rmi.RemoteException being thrown implicitly.
+ * </ul><p>
  * <b>NB:</b> The service interface is completely cajo project agnostic, as
  * all service definitions properly should be.
  *
@@ -74,8 +76,8 @@ package gnu.cajo;
  */
 public interface Service {
    /**
-    * used by servers to install proxies in client's local JVM, and by
-    * clients to install proxies in the server's local JVM. A proxy is a
+    * This class is both used by servers to install proxies in a client's JVM,
+    * and by clients to install proxies in a server's JVM. A proxy is a
     * serialisable object that on arrival at the target JVM, is initialised
     * with a reference to the service object, on which it can communicate.
     */
@@ -95,41 +97,45 @@ public interface Service {
     * code runs in the address space of the service JVM. A proxy enabled
     * service need not support client proxies, in which case a ClassNotFound
     * exception will be thrown implicitly.
-    * @param proxy an object to be instantiated at the service, which will
+    * <br><b>NB:</b> acceptance of client proxy code implies a significant
+    * amount of <u>trust</u> by the server, that the sent code
+    * is well behaved. However, client proxies can be made to run in an
+    * applet sandbox, as a compromise strategy.
+    * @param proxy an object to be sent to the service, which will
     * be provided a local reference to the service upon its arrival.
     * @return a remote reference to the proxy object, installed in the
-    * service JVM, or a server proxy to the reference, over which the client
-    * may interact.
-    * @throws ClassNotFoundException if the service does not accept
+    * service JVM, or a server proxy to the installed client proxy, over
+    * which the client may now interact.
+    * @throws ClassNotFoundException if the service JVM does not accept
     * client proxies.
     */
    Object sendProxy(Proxy proxy) throws ClassNotFoundException;
    /**
-    * used to request a client-side running, server proxy interface. It is
-    * considered an <i>important & common courtesy</i> of clients to request
-    * it, before attempting to use the service reference directly. It allows
-    * the service to potentially offload some computing load to the client,
-    * temporarily. It's a bit like the client asking: <i>May I help?</i>
-    * @return a local object supporting the service interface, which will be
-    * initialised on arrival, with a reference to its remote service. A
-    * client should not request a proxy unless it has enabled proxy
-    * acceptance, which is normally disabled by default; as this would cause a
-    * ClassNotFoundException to be thrown at the server, which must then be
-    * discarded by the server. <b>NB:</b> If a service does not support client
-    * proxies, it will return <i><u>null</u></i>.
+    * used to request a client-side running, server proxy. It is considered
+    * <i>common courtesy</i> of clients to request it, if their use scenario
+    * permits, before making use the service reference directly. It allows
+    * the service to potentially offload some computing or storage
+    * requirements to the client, temporarily. It's a bit like the client
+    * asking: <i>may I help with my requests?</i>
+    * @return a local object also supporting the service interface, which
+    * will be will be provided with a remote reference to its service.
+    * <br><b>NB:</b> If a service does not support client proxies, it will
+    * return <i><u>null</u></i>.
+    * @throws ClassNotFoundException if the client JVM does not accept
+    * service proxies.
     */
-   Proxy requestProxy();
+   Proxy requestProxy() throws ClassNotFoundException;
    /**
-    * whilst semantically unrelated to this service, is just too useful to
-    * leave out. Services typically exist as either development, or
-    * production. Most often, the <i>unreal</i>, i.e. test/demo services,
-    * are being used to validate infrastructure, communication, and
-    * performance, or to provide simulated functionality. Generally speaking,
-    * test/demo services interact with each other exclusively, and
-    * real/production services behave similarly; however, exceptions can be
-    * permitted...
-    * @return false if this service is a test or demonstration
-    * implementation, true if this is a production service.
+    * whilst semantically unrelated to this service, it is just too useful
+    * to leave out. Services typically exist as either development, or
+    * production. Most often, the <i>unreal</i>, i.e. test/demo/development
+    * services, are being used to validate infrastructure, communication,
+    * performance, provide simulated functionality, and validate functional
+    * correctness. Generally speaking, test/demo/development services
+    * interact with each other exclusively, and real/production services
+    * behave similarly. However, exceptions <i>can</i> be permitted...
+    * @return false if this service is a test demo or development
+    * implementation, else true if this is a production service.
     */
    boolean isReal();
 }
