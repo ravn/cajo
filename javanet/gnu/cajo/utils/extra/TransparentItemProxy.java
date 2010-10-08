cbf295e26eaee7d6af9f8c63386e4c0396aa3b71
John Catherino <cajo@dev.java.net>
2010-10-08 03:09:57 +0000

    documentation update: java.rmi.Remote is no longer implemented as one of the interfaces, as proxies may now be passed between JVMs, this discinction is irrelevant

--- javanet/gnu/cajo/utils/extra/TransparentItemProxy.java
+++ javanet/gnu/cajo/utils/extra/TransparentItemProxy.java
@@ -40,29 +40,23 @@ import java.lang.reflect.InvocationTargetException;
  */
 
 /**
- * This class creates an object, representing a service object. The returned
- * object will implement a list of interfaces defined by the client. The
- * interfaces are completely independent of interfaces the server object
- * implements, if any; they are simply logical groupings, of interest solely
- * to the client. The interface methods <i>'should'</i> declare that they
- * throw <tt>java.lang.Exception</tt>: However, the Java dynamic proxy
- * mechanism very <i>dubiously</i> allows this to be optional.
+ * This class creates an object, representing a local or remote service
+ * object. The returned object will implement a list of interfaces defined by
+ * the client. The interfaces are completely independent of interfaces the
+ * server object actually implements, if any; they are simply logical
+ * groupings of methods, of interest solely to the client.
  *
  * <p>Clients can dynamically create object proxy objects implementing
  * <i>any</i> combination of interfaces. Combining interfaces provides a very
  * important second order of abstraction. Whereas an interface is considered
  * to be a grouping of functionality; the <i>'purpose'</i> of an item, could
  * be determined by the group of interfaces it implements. Clients can
- * customise the way they use remote items as a function of the interfaces
- * implemented.
+ * customise the way they use local/remote objects as a function of the
+ * interfaces implemented.
  *
  * <p>The proxy instances returned from this class are serialisable. Proxies
  * can be persisted to storage for later use, and passed to other JVMs over
  * the network.
- *
- * <p>If an item can be best represented with a single interface, it might be
- * well to consider using a {@link Wrapper Wrapper} class instead. It is
- * conceptually much simpler, though lacks the syntactical transparency.
  *
  * <p><i><u>Note</u>:</i> Unfortunately, this class only works with JREs 1.3
  * and higher. Therefore I was reluctant to include it in the official
@@ -73,7 +67,7 @@ import java.lang.reflect.InvocationTargetException;
  * <p><b>Update:</b> For users of JRE 1.5+<br>
  * It you wish to invoke time consuming methods asynchronously, simply
  * declare that the method returns a <a href=http://download.oracle.com/javase/6/docs/api/java/util/concurrent/Future.html>
- * java.util.Concurrent.Future of the required return type. The invocation
+ * java.util.Concurrent.Future</a> of the required return type. The invocation
  * will return immediately, and the future will contain the result when the
  * invocation is completed. You can then peridocally check to see if it is
  * done, and extract the result.
@@ -146,33 +140,34 @@ public final class TransparentItemProxy implements
     * It is expected that this object will implement a method of the following
     * signature:<p>
     * <blockquote><tt>
-    * public Object handle(Object item, String method, Object args[] Throwable t)
+    * public Object handle(Object item, String method, Object args[],
+    * Throwable t)
     * throws Exception; </tt></blockquote><p>
     * The first arguments are as follows:<ul>
-    * <li>the remote item reference on which the method was being invoked
+    * <li>the item reference on which the method was being invoked
     * <li>the name of the method that was called on the remote object
     * <li>the arguments that were provided in the method call
     * <li>the error that resulted from the invocation</ul>
     * The handler will either successfully recover from the error, and return
-    * the appropriate result, or throw a hopefully more descriptive exception.
+    * a substitute result, or throw a hopefully more descriptive exception.
     */
    public static Object handler;
    /**
     * This method, inherited from InvocationHandler, simply passes all object
-    * method invocations on to the remote object, automatically and
-    * transparently. This allows the local runtime to perform remote item
+    * method invocations on to the wrapped object, automatically and
+    * transparently. This also allows the local runtime to perform remote item
     * invocations, while appearing syntactically identical to local ones.
     * @param proxy The locallly created proxy object on which the method was
     * originally invoked.
     * @param method The method to invoke on the object, in this case the
-    * server item.
+    * local or remote service object.
     * @param args The arguments to provide to the method, if any.
     * @return The resulting data from the method invocation, if any.
     * @throws java.rmi.RemoteException For network communication related
-    * reasons.
+    * reasons with a remote service.
     * @throws NoSuchMethodException If no matching method can be found
-    * on the server item.
-    * @throws Exception If the server item rejected the invocation, for
+    * on the service object.
+    * @throws Exception If the service object rejected the invocation, for
     * application specific reasons.
     */
    public Object invoke(Object proxy, Method method, final Object args[])
@@ -232,28 +227,19 @@ public final class TransparentItemProxy implements
       }
    }
    /**
-    * This generates a class definition for a remote object reference at
+    * This generates a class definition for a any object reference at
     * runtime, and returns a local object instance. The resulting dynamic
-    * proxy object will implement all the interfaces provided. <i><u>Note</u>:</i>
-    * if this proxy object is to be passed between JVMs, the item <i>must</i>
-    * be either serialisable (as a completely local proxy) or be wrapped in a
-    * gnu.cajo.invoke.Remote wrapper.
-    * @param item A reference to a <i>presumably</i> remote server object;
-    * a local object instance could be used as well; to distinguish between
-    * these two cases, proxies to remote objects will also implement the
-    * marker interface java.rmi.Remote, on which the proxy may be tested via
-    * the instanceof operator. <i><u>Note</u>:</i> a non-serialisable local
-    * item will be automatically remoted when serialised, so as to allow the
-    * proxies to be passed between JVMs.
+    * proxy object will implement all the interfaces provided.
+    * @param item A reference to a either a remote object, or a local one<br>
+    * <i><u>Note</u>:</i> a non-serialisable local item will be automatically
+    * remoted when serialised, so as to support proxies being freely passed
+    * between JVMs.
     * @param interfaces The list of interface classes for the dynamic proxy
     * to implement. Typically, these are provided thus; <tt>new Class[] {
     * Interface1.class, Interface2.class, ... }</tt>
-    * @return A reference to the server item, wrapped in the local object,
-    * created at runtime. It can then be typecast into any of the interfaces,
-    * as needed by the client. <i><u>Note</u>:</i> if the item reference is
-    * to an object in a remote JVM, the returned proxy will <i>also</i>
-    * implement the marker interface java.rmi.Remote, to allow this fact to be
-    * easily tested, via the instanceof operator.
+    * @return A reference to the provided object, wrapping the local object,
+    * it can then be typecast into any of the interfaces, as needed by the
+    * client.
     */
    public static Object getItem(Object item, Class interfaces[]) {
       return Proxy.newProxyInstance(interfaces[0].getClassLoader(),
@@ -272,7 +258,7 @@ public final class TransparentItemProxy implements
     * @param interfaces The list of interface classes for the dynamic proxy
     * to implement. Typically, these are provided thus; <tt>new Class[] {
     * Interface1.class, Interface2.class, ... }</tt>
-    * @return A reference to the server item, wrapped in the object created
+    * @return A reference to the server item, wrapping the object created
     * at runtime, implementing all of the interfaces provided. It can then
     * be typecast into any of the interfaces, as needed by the client.
     * @throws RemoteException if the remote registry could not be reached.
